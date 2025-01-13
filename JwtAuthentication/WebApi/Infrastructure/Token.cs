39e0aad3a61a6c4bc09fc54865468d3307a679d6
Welington Dias <33883532+Welin7@users.noreply.github.com>
2025-01-12 21:03:35 -0300

    Update dtosof token and refresh token.

--- JwtAuthentication/WebApi/Infrastructure/Token.cs
+++ JwtAuthentication/WebApi/Infrastructure/Token.cs
@@ -1,7 +1,10 @@
-﻿namespace WebApi.Infrastructure
+﻿using WebApi.Models;
+
+namespace WebApi.Infrastructure
 {
     public class Token
     {
         public string AccessToken { get; set; } = string.Empty;
+        public RefreshToken RefreshToken { get; set; }
     }
 }
---
+++ JwtAuthentication/WebApi/Models/RefreshToken.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.Models
+{
+    public class RefreshToken
+    {
+        public required string Token { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        public DateTime Expires { get; set; }
+        public bool Enabled { get; set; }
+        public string Email { get; set; } = string.Empty;
+    }
+}
